6429aacfb0fabf6a1a2455cbff6b9596d7a34738
Mohammed Karim <mkarom@jpl.nasa.gov>
2018-08-23 18:43:26 +0000

    updated dockerfile to add ariamh

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,5 +3,5 @@
 # clone spyddder-man to be moved to its final location by docker builder
 git clone https://github.com/hysds/spyddder-man.git
 
-# clone slcp2pm to be moved to its final location by docker builder
-#git clone https://github.com/hysds/slcp2pm.git
+# clone ariamh to be moved to its final location by docker builder
+git clone -b standard-product https://github.com/mkarim2017/ariamh.git
