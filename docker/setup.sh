53721ecbdbc2b5ed0f1f8c3ddd65e8565ddde083
Mohammed Karim <mkarom@jpl.nasa.gov>
2018-10-03 00:25:44 +0000

    adding lightweight_water_mask

--- docker/setup.sh
+++ docker/setup.sh
@@ -6,5 +6,3 @@ git clone https://github.com/hysds/spyddder-man.git
 # clone ariamh to be moved to its final location by docker builder
 git clone -b standard-product https://github.com/mkarim2017/ariamh.git
 
-#clone lightweight_water_mask
-git clone https://github.jpl.nasa.gov/aria-hysds/lightweight_water_mask.git
