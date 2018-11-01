5238c23e8c5b6ea8adb845064f37396c29c424ce
Mohammed Karim <mkarom@jpl.nasa.gov>
2018-11-01 13:30:44 +0000

    update

--- sling_acquisitions.py
+++ sling_acquisitions.py
@@ -783,7 +783,7 @@ def submit_sling_job(project, spyddder_extract_version, acquisition_localizer_ve
     """Map function for spyddder-man extract job."""
 
     acquisition_localizer_version = "standard-product"
-
+    spyddder_extract_version = "standard-product"
     job_submit_url = '%s/mozart/api/v0.1/job/submit' % MOZART_URL
 
     # set job type and disk space reqs
