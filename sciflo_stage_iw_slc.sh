ed5353312e5c1b2d2c92d2f7cadb076e1269fc24
Mohammed Karim <mkarom@jpl.nasa.gov>
2018-11-10 00:54:49 +0000

    update

--- sciflo_stage_iw_slc.sh
+++ sciflo_stage_iw_slc.sh
@@ -3,16 +3,9 @@ BASE_PATH=$(dirname "${BASH_SOURCE}")
 BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 
 # source ISCE env
-export GMT_HOME=/usr/local/gmt
-export ARIAMH_HOME=$HOME/ariamh
 export STANDARD_PRODUCT_HOME=$HOME/standard_product
-source $ARIAMH_HOME/isce.sh
-source $ARIAMH_HOME/giant.sh
-export TROPMAP_HOME=$HOME/tropmap
-export UTILS_HOME=$ARIAMH_HOME/utils
-#export GIANT_HOME=/usr/local/giant/GIAnT
-export PYTHONPATH=$BASE_PATH:$ARIAMH_HOME:$PYTHONPATH
-export PATH=$BASE_PATH:$TROPMAP_HOME:$GMT_HOME/bin:$PATH
+export PYTHONPATH=$BASE_PATH:$PYTHONPATH
+export PATH=$BASE_PATH:$PATH
 
 # source environment
 source $HOME/verdi/bin/activate
