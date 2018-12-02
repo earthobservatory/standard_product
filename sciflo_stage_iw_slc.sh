222b263943008fccd40a8c750880f874e96a5053
gmanipon <gmanipon@jpl.nasa.gov>
2018-12-02 07:03:47 +0000

    export proper env variables

--- sciflo_stage_iw_slc.sh
+++ sciflo_stage_iw_slc.sh
@@ -2,13 +2,25 @@
 BASE_PATH=$(dirname "${BASH_SOURCE}")
 BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 
-# source ISCE env
-export STANDARD_PRODUCT_HOME=$HOME/standard_product
+# source PGE env
 export PYTHONPATH=$BASE_PATH:$PYTHONPATH
 export PATH=$BASE_PATH:$PATH
+export PYTHONPATH=${PYTHONPATH}:${HOME}/verdi/etc
+# source ISCE env
+export GMT_HOME=/usr/local/gmt
+export ARIAMH_HOME=$HOME/ariamh
+export STANDARD_PRODUCT_HOME=$HOME/standard_product
+source $ARIAMH_HOME/isce.sh
+source $ARIAMH_HOME/giant.sh
+export TROPMAP_HOME=$HOME/tropmap
+export UTILS_HOME=$ARIAMH_HOME/utils
+export GIANT_HOME=/usr/local/giant/GIAnT
+export PYTHONPATH=${HOME}/verdi/etc:$ISCE_HOME/applications:$ISCE_HOME/components:$BASE_PATH:$ARIAMH_HOME:$TROPMAP_HOME:$GIANT_HOME:$PYTHONPATH
+export PATH=$BASE_PATH:$TROPMAP_HOME:$GMT_HOME/bin:$PATH
 
 # source environment
 source $HOME/verdi/bin/activate
+
 
 echo "##########################################" 1>&2
 echo -n "Running S1 create interferogram sciflo: " 1>&2
